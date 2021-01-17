13576e404d94b7fa33b7b8b0d73a82b7c16db37e
AslamHossain <aslamecehstu43java@gmail.com>
2021-01-17 21:18:12 +0600

    Update package

---
+++ Codeforces/src/Watermelon.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class Watermelon {
+	public static void main(String[] args) {
+		int w;
+		Scanner sc = new Scanner(System.in);
+		w = sc.nextInt();
+		if (w > 2) {
+			if ((w % 2) == 0)
+				System.out.println("YES");
+			else
+				System.out.println("NO");
+		} else
+			System.out.println("NO");
+	}
+}
